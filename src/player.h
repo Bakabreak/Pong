6b1b7e7893b90c607accdf849693012436400c61
Qiu Le Chen <q.l.chen@student.tue.nl>
2022-07-09 14:54:22 +0200

    add player

---
+++ src/player.h
@@ -0,0 +1,20 @@
+//
+// Created by Qiu Le Chen on 09-Jul-22.
+//
+
+
+class Player{
+public:
+    float posX;
+    float posY;
+    int playerId;
+    float width;
+    float height;
+    explicit Player(int playerId);
+    void setWidth(float width);
+    void setHeight(float height);
+    float getPosX();
+    float getPosY();
+    void updatePos(float xNew, float yNew);
+    void renderPlayer();
+};
