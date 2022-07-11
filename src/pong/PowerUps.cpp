9d1ee664690aea8b60db56f520b1ff0371776558
Qiu Le Chen <q.l.chen@student.tue.nl>
2022-07-11 14:33:35 +0200

    add powerUps files

---
+++ src/pong/PowerUps.cpp
@@ -0,0 +1,9 @@
+//
+// Created by Qiu Le Chen on 11-Jul-22.
+//
+
+#include "PowerUps.h"
+
+void PowerUps::expand() {
+
+}
---
+++ src/pong/PowerUps.h
@@ -0,0 +1,21 @@
+//
+// Created by Qiu Le Chen on 11-Jul-22.
+//
+
+#ifndef PONG_POWERUPS_H
+#define PONG_POWERUPS_H
+
+
+class PowerUps {
+    void expand();
+
+    void multiply();
+
+    void accelerate();
+
+    void curve();
+
+};
+
+
+#endif //PONG_POWERUPS_H
