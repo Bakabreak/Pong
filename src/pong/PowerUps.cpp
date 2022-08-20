a4127629fcb27a80ca8213475b91aa798d62e719
Qiu Le Chen <q.l.chen@student.tue.nl>
2022-08-20 22:38:02 +0200

    Add constructor and "expand" functionality

--- src/pong/PowerUps.cpp
+++ src/pong/PowerUps.cpp
@@ -3,6 +3,25 @@
 //
 
 #include "PowerUps.h"
+#include "CollisionBox.h"
+#include "Ball.h"
+#include <GLFW/glfw3.h>
+#include <cmath>
+
+PowerUps::PowerUps(float aWidth, float aHeight) {
+    this->setWidth(aWidth);
+    this->setHeight(aHeight);
+    this->setTag("powerUp");
+}
+
+void PowerUps::setPos(float xPos, float yPos) {
+    this->posY = yPos;
+    this->posX = xPos;
+}
+
+void PowerUps::expand(Ball *ball) {
+    ball->setRadius(ball->radius * 2);
+}
 
 void PowerUps::expand() {
 
