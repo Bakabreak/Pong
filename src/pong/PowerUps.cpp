8cf17d7ec0a8956d1aebf7c76227d315b920c450
Qiu Le Chen <q.l.chen@student.tue.nl>
2022-08-20 22:38:32 +0200

    Add render, and "multiply"

--- src/pong/PowerUps.cpp
+++ src/pong/PowerUps.cpp
@@ -23,6 +23,27 @@ void PowerUps::expand(Ball *ball) {
     ball->setRadius(ball->radius * 2);
 }
 
-void PowerUps::expand() {
+Ball PowerUps::multiply(Ball ball) {
+    return ball.clone();
+}
+
+void PowerUps::accelerate(CollisionBox object) {}
+
+void PowerUps::render() {
+    int vertMax = 2;
+    float vertAngle = 0;
+    float pi = 3.141592654;
+
+    glBegin(GL_POLYGON);
+    glColor3f(1, 1, 1);
+    for (int i = 0; i < vertMax; i++) {
+        glVertex2d(this->posX + cos(vertAngle) * this->width / 2,
+                   this->posY + sin(vertAngle + pi / 2) * this->height / 2);
+        glVertex2d(this->posX + cos(vertAngle) * this->width / 2,
+                   this->posY - sin(vertAngle + pi / 2) * this->height / 2);
+        vertAngle += 2 * pi / vertMax;
+    }
+    glEnd();
+}
 
 }
